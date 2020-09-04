fed6c179bcd07788f6328e61a2b06dffd418c9ab
Brandon Wheatley <brandon.a.wheatley@gmail.com>
2020-09-04 11:07:38 -0500

    Created all crew member classes

--- Bank.cs
+++ Bank.cs
@@ -2,17 +2,19 @@ namespace heist2
 {
     public class Bank
     {
-        private int CashOnHand {get; set;}
-        private int AlarmScore {get; set;}
-        private int VaultScore {get; set;}
-        private int SecurityGuardScore {get; set;}
+        public int CashOnHand { get; set; }
+        public int AlarmScore { get; set; }
+        public int VaultScore { get; set; }
+        public int SecurityGuardScore { get; set; }
 
-        private bool IsSecure() 
+        private bool IsSecure()
         {
             if (AlarmScore <= 0 && VaultScore <= 0 && SecurityGuardScore <= 0)
             {
                 return false;
-            } else {
+            }
+            else
+            {
                 return true;
             }
         }
---
+++ Muscle.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace heist2
+{
+    public class Muscle : IRobber
+    {
+        public string Name { get; set; }
+        public int SkillLevel { get; set; }
+
+        public int PercentageCut { get; set; }
+
+        public void PerformSkill(Bank bank)
+        {
+            bank.SecurityGuardScore = bank.SecurityGuardScore - SkillLevel;
+            Console.WriteLine($"{Name} is knockin' noggins. Decreased security by {SkillLevel}.");
+            if (bank.SecurityGuardScore <= 0)
+            {
+                Console.WriteLine($"{Name} has clobbered all the guards!");
+            }
+        }
+    }
+}
